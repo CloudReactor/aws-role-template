c6a4328df500498d3d181f6348a9f5d05a7ca669
jtsay <jctsay@kbs-services.com>
2019-11-05 00:25:28 -0800

    Use a Jinja2 template to generate the output

---
+++ make_template.py
@@ -0,0 +1,24 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(
+    loader=FileSystemLoader('./')
+)
+
+template = env.get_template('oneprocdash-aws-role-template.json.j2')
+
+with open('url_requester.py', 'r') as f:
+    url_requester_contents = f.read()
+
+data = {
+  'url_requester_contents': json.dumps(url_requester_contents)
+}
+
+output = template.render(data)
+
+output_filename = 'oneprocdash-aws-role-template.json'
+
+with open(output_filename, 'w') as f:
+    f.write(output)
+
+print(f"Wrote file '{output_filename}' with CloudFormation template.")
